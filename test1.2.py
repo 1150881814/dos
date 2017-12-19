8e95d5d0c7acc477fb9db57b424d029d6ce7fd5a
1150881814 <1150881814@qq.com>
2017-12-19 21:39:24 +0800

    1

--- test1.2.py
+++ test1.2.py
@@ -1,3 +1,7 @@
 print("What's your name?")
-name = input()
-print("Your name is %d" % name)
+name = str(input())
+for a in range(0,5):
+    for b in range(0,5):
+        print("%s傻逼" % name,end='')
+    print("%s傻逼" % name)
+
--- test1.3.py
+++ test1.3.py
@@ -1,9 +1,6 @@
-result = 0
-answer = False
-for a in range(1,101):
-    if answer == False:
-        result = result + a
-
-    if a == 100:
-        answer == True
-        print(result)
+c = 0
+d = 0
+result = False
+for c in  range(1,101):
+    d = c + d
+print(d)
