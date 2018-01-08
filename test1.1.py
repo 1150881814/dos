f2b25892d5979c26d8eae7f1d661e07f0a24ec04
1150881814 <1150881814@qq.com>
2018-01-08 16:09:28 +0800

    1

--- test1.1.py
+++ test1.1.py
@@ -2,15 +2,28 @@ from random import  randint
 number = randint(1,100)
 win = False
 print("guess")
+winpoint = 3
 while win == False:
     answer = int(input())
 
     if answer > number:
         print("%d is too big" % answer)
+        winpoint = winpoint - 1
+        if winpoint != 0:
+            print("You still have %d chance"% winpoint)
+        if winpoint == 0:
+            print("You lose")
+            break
 
     if answer < number:
         print("%d is too small" % answer)
+        winpoint = winpoint - 1
+        if winpoint != 0:
+            print("You still have %d chance"% winpoint)
+        if winpoint == 0:
+            print("You lose")
+            break
 
     if answer == number:
         print("Excellent! %d is the right answer" % answer)
-        win == True
+        break
